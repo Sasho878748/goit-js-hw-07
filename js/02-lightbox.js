667fda80a47c67aa76628d99ac02602ba018e0fe
Sasho878748 <kovalenko878748@gmail.com>
2023-04-22 14:01:59 +0300

    final hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `
+    )
+    .join("");
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+});
