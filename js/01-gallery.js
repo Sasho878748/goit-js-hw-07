7d5facccf9c5349880e06a01c8bcc098936a2127
Sasho878748 <kovalenko878748@gmail.com>
2023-04-22 12:22:38 +0300

    new

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,4 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-
-// const img = document.querySelectorAll(".gallery__image");
-
-gallery.addEventListener('click', onGalleryClick);
-
-function onGalleryClick(event) {
-  event.preventDefault();
-  
-  const target = event.target;
-  
-  if (target.nodeName !== 'IMG') {
-    return;
-  }
-  
-  const largeImageUrl = target.dataset.source;
-  
-  openModal(largeImageUrl);
-}
-
+console.log(galleryItems);
