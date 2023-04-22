f2ff533c5df4853e65daca511841f84d365ad328
Sasho878748 <kovalenko878748@gmail.com>
2023-04-22 13:43:29 +0300

    final task 1 hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>
+  `
+    )
+    .join("");
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName !== "IMG") {
+    return;
+  }
+
+  const largeImageSrc = clickedEl.dataset.source;
+  const instance = basicLightbox.create(`
+      <img src="${largeImageSrc}" width="800" height="600">
+    `);
+  instance.show();
+
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && instance.visible()) {
+      instance.close();
+      document.body.classList.remove("is-open");
+    }
+  });
+});
